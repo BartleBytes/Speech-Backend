cc8f5646b5f39d89e3d428198fb396db49762e05
dylanbartle <dylanbartle@hotmail.com>
2025-05-19 20:08:20 -0600

    router

--- routes/register.js
+++ routes/register.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { handleRegistration } = require('../controllers/registerController');
+const { handleRegistration } = require('../controllers/register');
 
 router.post('/', handleRegistration);
 
