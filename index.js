f86686bc5c17c9b21421e786b3b0f3ef73164471
dylanbartle <dylanbartle@hotmail.com>
2025-05-19 20:35:05 -0600

    get

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const registerRoute = require('./routes/register');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 5010;
@@ -8,7 +9,7 @@ const PORT = process.env.PORT || 5010;
 const corsOptions = {
     origin: 'https://empowerspeech.netlify.app',
     methods: ['GET', 'POST'],
-    credentials: true,
+    credentials: true
 };
 
 
@@ -21,6 +22,10 @@ app.get('/', (req, res) => {
     res.send('Speech Camp Backend is running');
   });
   
+app.get('/api/registrations', (req, res) => {
+  const filePath = path.join(__dirname, 'data', registrations.json)
+  res.sendFile(filePath)
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
